c919117bc272e8484c1579990ddb3cb2e0cd80db
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2023-10-17 17:12:34 +0200

    progreso 17/10/2023

--- BOOK4U/src/backend/db.java
+++ BOOK4U/src/backend/db.java
@@ -19,6 +19,8 @@ public class db {
 
 	private static final Connection con = conectarBD();
 
+	//funcion para conectar a la base de datos
+	//no pide nada y devuelde una conexion a la base de datos de la aplicacion
 	public static Connection conectarBD() {
 		Connection con = null;
 
@@ -32,6 +34,29 @@ public class db {
 		}
 
 		return con;
+	}
+	
+	public static void crearCliente(String dni, String nombre, String apellidos, int telefono, String correo, String password) {
+		String sql = "INSERT INTO cliente value("+dni+","+nombre+","+apellidos+","+telefono+",,"+correo+","+password+",)";
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+	}
+	
+	public static void editarCliente(String dni, String nombre, String apellidos, int telefono, String correo, String password, int creditos) {
+		String sql = "UPDATE TABLE cliente SET dni = "+dni+", nombre = "+nombre+", apellidos = "+apellidos+", telefono = "+telefono+", , password = "+password+" WHERE dni = "+dni+";";
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
 
 	}
 }
