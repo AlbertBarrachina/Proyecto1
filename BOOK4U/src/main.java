1a22490181996915ead11c23a2edb8e56a27fcdd
APEREZ2002 <119208468+APEREZ2002@users.noreply.github.com>
2023-10-19 17:07:31 +0200

    Add files via upload

--- BOOK4U/src/main.java
+++ BOOK4U/src/main.java
@@ -1,19 +1,18 @@
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import java.awt.Font;
-import java.awt.Color;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 public class main extends JFrame {
 
     public main() {
         // Crear el panel con el BorderLayout y ajustar su tamaño
         JPanel panel = new JPanel(new BorderLayout());
-        panel.setSize(150, 100);
+        panel.setSize(350, 100);
+
+        // Agregar un borde al panel
+        Border panelBorder = new LineBorder(Color.BLACK); // Puedes personalizar el color y el estilo del borde
+        panel.setBorder(panelBorder);
 
         // Crear los componentes y ajustar su tamaño
         Font font = new Font("Arial", Font.PLAIN, 12);
@@ -24,6 +23,11 @@ public class main extends JFrame {
 
         JPanel topPanel = new JPanel(new BorderLayout());
         topPanel.setSize(150, 25); // Reducir el tamaño del panel superior
+
+        // Agregar un borde a topPanel
+        Border topPanelBorder = new LineBorder(Color.BLACK); // Puedes personalizar el color y el estilo del borde
+        topPanel.setBorder(topPanelBorder);
+
         JLabel titulo = new JLabel("TITULO");
         titulo.setHorizontalAlignment(JLabel.CENTER);
         titulo.setFont(font);
@@ -38,50 +42,78 @@ public class main extends JFrame {
         // Crear un panel para el botón de perfil con forma circular en la parte superior derecha
         JPanel profileButtonPanel = new JPanel(new BorderLayout());
         profileButtonPanel.setSize(75, 75); // Tamaño del botón circular
-        JButton perfil = new JButton("Perfil");
+
+        // Agregar un borde a profileButtonPanel
+        Border profileButtonPanelBorder = new LineBorder(Color.BLACK); // Puedes personalizar el color y el estilo del borde
+        profileButtonPanel.setBorder(profileButtonPanelBorder);
+
+        JButton perfil = new JButton("Perfil") {
+            @Override
+            protected void paintComponent(Graphics g) {
+                if (!isOpaque()) {
+                    super.paintComponent(g);
+                    return;
+                }
+
+                super.paintComponent(g);
+
+                int width = getWidth();
+                int height = getHeight();
+                g.setColor(getBackground());
+                g.fillOval(0, 0, width, height);
+            }
+        };
+
         perfil.setFont(font);
         perfil.setForeground(Color.WHITE); // Color del texto en el botón
         perfil.setBackground(Color.BLUE); // Color de fondo del botón
         perfil.setOpaque(true);
         perfil.setBorderPainted(false);
         perfil.setSize(75, 75); // Tamaño del botón circular
-        profileButtonPanel.add(perfil, BorderLayout.EAST);
+        profileButtonPanel.add(perfil, BorderLayout.NORTH);
         panel.add(profileButtonPanel, BorderLayout.EAST);
 
-        // Crear un panel para los contenidos centrados en tres columnas
-        JPanel contentPanel = new JPanel(new GridLayout(2, 3)); // 2 filas y 3 columnas
-        contentPanel.setSize(150, 50); // Aumentar el tamaño del panel intermedio para acomodar los contenidos
+        // Crear un panel para los contenidos centrados en tres columnas usando GridBagLayout
+        JPanel contentPanel = new JPanel(new GridBagLayout());
 
-        JLabel contenido1 = new JLabel("CONTENIDO");
-        contenido1.setFont(font);
-        contentPanel.add(contenido1);
+        // Agregar un borde a contentPanel
+        Border contentPanelBorder = new LineBorder(Color.BLACK); // Puedes personalizar el color y el estilo del borde
+        contentPanel.setBorder(contentPanelBorder);
 
-        JLabel contenido2 = new JLabel("CONTENIDO");
-        contenido2.setFont(font);
-        contentPanel.add(contenido2);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0; // Aumentar el peso horizontal para desplazar hacia la derecha
+        gbc.insets = new Insets(25, 40, 25, 40); // Espaciado entre componentes (más margen a la derecha)
 
-        JLabel contenido3 = new JLabel("CONTENIDO");
-        contenido3.setFont(font);
-        contentPanel.add(contenido3);
+        for (int i = 0; i < 6; i++) {
+            JPanel contentItem = new JPanel();
+            contentItem.setLayout(new BoxLayout(contentItem, BoxLayout.Y_AXIS));
 
-        JLabel contenido4 = new JLabel("CONTENIDO");
-        contenido4.setFont(font);
-        contentPanel.add(contenido4);
+            // Agregar una etiqueta para la imagen
+            ImageIcon imageIcon = new ImageIcon("descarga.jfif"); // Reemplaza con la ruta de tu imagen
+            JLabel imageLabel = new JLabel(imageIcon);
+            contentItem.add(imageLabel);
 
-        JLabel contenido5 = new JLabel("CONTENIDO");
-        contenido5.setFont(font);
-        contentPanel.add(contenido5);
+            // Agregar dos etiquetas de descripción
+            JLabel descriptionLabel1 = new JLabel("Descripción 1");
+            descriptionLabel1.setFont(font);
+            contentItem.add(descriptionLabel1);
 
-        JLabel contenido6 = new JLabel("CONTENIDO");
-        contenido6.setFont(font);
-        contentPanel.add(contenido6);
+            JLabel descriptionLabel2 = new JLabel("Descripción 2");
+            descriptionLabel2.setFont(font);
+            contentItem.add(descriptionLabel2);
+
+            gbc.gridx = i % 3;
+            gbc.gridy = i / 3;
+            contentPanel.add(contentItem, gbc);
+        }
 
         panel.add(contentPanel, BorderLayout.CENTER);
 
         // Ajustar el tamaño de la ventana
         this.add(panel);
-        this.setTitle("PAGINA PRINCIPAL");
-        this.setSize(300, 150); // Aumentar el tamaño de la ventana para mostrar mejor los contenidos
+        this.setTitle("BOOK4U");
+        this.setSize(800, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
