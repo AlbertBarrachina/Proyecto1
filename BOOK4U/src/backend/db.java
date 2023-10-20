5fa76bd2c758f70e16d6644e4f97af886e54ab48
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2023-10-20 16:22:53 +0200

    introducidos datos de conexion

--- BOOK4U/src/backend/db.java
+++ BOOK4U/src/backend/db.java
@@ -9,18 +9,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class db {
-	
-	private static final String USER = "";
-	private static final String PWD = "";
-	//conexionn dentro de ilerna
+
+	private static final String USER = "DW2_2324_BOOK4U_ASA";
+	private static final String PWD = "AASA";
+	// conexionn dentro de ilerna
 	private static final String URL = "jdbc:oracle:thin:@192.168.3.26:1521:xe";
-	//conexion fuera de ilerna
+	// conexion fuera de ilerna
 //	private static final String URL = "jdbc:oracle:thin:@oracle.ilerna.com:1521:xe";
 
 	private static final Connection con = conectarBD();
 
-	//funcion para conectar a la base de datos
-	//no pide nada y devuelde una conexion a la base de datos de la aplicacion
+	// funcion para conectar a la base de datos
+	// no pide nada y devuelde una conexion a la base de datos de la aplicacion
 	public static Connection conectarBD() {
 		Connection con = null;
 
@@ -35,25 +35,29 @@ public class db {
 
 		return con;
 	}
-	
-	public static void crearCliente(String dni, String nombre, String apellidos, int telefono, String correo, String password) {
-		String sql = "INSERT INTO cliente value("+dni+","+nombre+","+apellidos+","+telefono+",,"+correo+","+password+",)";
+
+	public static void crearCliente(String dni, String nombre, String apellidos, int telefono, String correo,
+			String contrasenya) {
+		String sql = "INSERT INTO cliente value(" + dni + "," + nombre + "," + apellidos + "," + telefono + ",'',"
+				+ correo + "," + contrasenya + ",0,'S')";
 		try {
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			
+
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
 
 	}
-	
-	public static void editarCliente(String dni, String nombre, String apellidos, int telefono, String correo, String password, int creditos) {
-		String sql = "UPDATE TABLE cliente SET dni = "+dni+", nombre = "+nombre+", apellidos = "+apellidos+", telefono = "+telefono+", , password = "+password+" WHERE dni = "+dni+";";
+
+	public static void editarCliente(String dni, String nombre, String apellidos, int telefono, String correo,
+			String contrasenya, int creditos) {
+		String sql = "UPDATE TABLE cliente SET dni = " + dni + ", nombre = " + nombre + ", apellidos = " + apellidos
+				+ ", telefono = " + telefono + ", , password = " + contrasenya + " WHERE dni = " + dni + ";";
 		try {
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			
+
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
