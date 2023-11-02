376a01e8c9cc4c9bcc9dbf33c895b433c597a946
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2023-11-02 16:31:15 +0100

    función leertexto corregida
    Ahora funciona.

--- BOOK4U/src/backend/archivo.java
+++ BOOK4U/src/backend/archivo.java
@@ -7,13 +7,15 @@ import java.io.IOException;
 
 public class archivo {
 
+	
+	//lee el archivo introducido en la variable "ruta" y solo la linea "linea", las lineas empiezan por 1 y la ruta para entrar en la carpeta config es "./src/config/"
 	public static String[] leerTxt(String ruta, int linea) {
 		String[] lista = new String[10];
 		int i = 0;
 		int n = 0;
 		try {
 			File archivo = new File(ruta);
-
+			
 			FileReader fileReader = new FileReader(archivo);
 
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -22,10 +24,11 @@ public class archivo {
 			while ((textolinea = bufferedReader.readLine()) != null) {
 				i++;
 				if (i == linea) {
-					String[] tokens = textolinea.split("[;]");
+					String[] tokens = textolinea.split(";");
 
 					for (String token : tokens) {
 						lista[n]=(token.trim());
+						n++;
 					}
 					break;
 				}
--- BOOK4U/src/main.java
+++ BOOK4U/src/main.java
@@ -1,12 +1,13 @@
 import java.security.NoSuchAlgorithmException;
 
+import backend.archivo;
 import backend.db;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String cliente[] = new String[9];
+		String cliente[] = new String[10];
 //		System.out.println(db.crearCliente("Alex", "Perezzzzzzz", 444522636, "perezzz@gmail.con", "cowontraseña"));
 //		cliente = db.mostrarInfoCliente(1);
 //		System.out.println(cliente[0]+cliente[1]+cliente[2]+cliente[3]+cliente[4]+cliente[5]+cliente[6]+cliente[7]+cliente[8]);
@@ -15,7 +16,10 @@ public class main {
 //		}else {
 //			System.out.println("wawaa");
 //		}
-		System.out.println(db.editarInfoCliente(2, "NOT Alex", "Prieto", 669696976, "correogenrico@gmail.com"));
+//		System.out.println(db.editarInfoCliente(2, "NOT Alex", "Prieto", 669696976, "correogenrico@gmail.com"));
+		cliente = archivo.leerTxt("./src/config/texto.txt", 1);
+		System.out.println(cliente[0]);
+		System.out.println(cliente[1]);
 	}
 
 }
