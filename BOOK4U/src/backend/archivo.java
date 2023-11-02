1352caee810d2cb2afc1666656babfb6563b1891
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2023-11-02 16:14:14 +0100

    subida clase con funcion para leer txt

---
+++ BOOK4U/src/backend/archivo.java
@@ -0,0 +1,42 @@
+package backend;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class archivo {
+
+	public static String[] leerTxt(String ruta, int linea) {
+		String[] lista = new String[10];
+		int i = 0;
+		int n = 0;
+		try {
+			File archivo = new File(ruta);
+
+			FileReader fileReader = new FileReader(archivo);
+
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			String textolinea;
+			while ((textolinea = bufferedReader.readLine()) != null) {
+				i++;
+				if (i == linea) {
+					String[] tokens = textolinea.split("[;]");
+
+					for (String token : tokens) {
+						lista[n]=(token.trim());
+					}
+					break;
+				}
+			}
+
+			bufferedReader.close();
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return lista;
+	}
+}
